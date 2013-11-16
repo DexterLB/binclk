0940a9dfcff8a670e592def4bb466912421ac1cd
Angel Angelov <hextwoa@gmail.com>
2013-11-16 20:28:21 +0200

    got dst table generation working

---
+++ src/dst/dst.c
@@ -0,0 +1,74 @@
+/*
+ * generates a DST table (the time_t values at which the DST state changes)
+ * takes 3 arguments: start, end and delta:
+ *      start is the start of the period
+ *      end is.. forget it
+ *      delta is the step, in which it is guaranteed that no more than 1
+ *          dst changes will occur.
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool is_dst(time_t t)
+{
+    struct tm *time;
+    time = localtime(&t);
+    return (time->tm_isdst > 0);
+}
+
+time_t search(time_t start, time_t end)
+// find a single DST change within a period, or return 0 if there are no
+// changes within the period
+{
+    time_t half;
+    if (is_dst(start) == is_dst(end))
+        return 0;   // nothing to look for in this interval
+
+    // binary search
+    while (end - start > 1) {
+        half = start + ((end - start) / 2);
+
+        if (is_dst(start) == is_dst(half))
+            start = half;
+        else if (is_dst(end) == is_dst(half))
+            end = half;
+    }
+    return end;
+}
+
+void print_time(time_t time)
+{
+    printf("%llu %s\n", (unsigned long long)time, (is_dst(time) ? "on" : "off"));
+}
+
+void multisearch(time_t start, time_t end, time_t delta)
+// perform multiple searches in successive periods, defined by delta,
+// until end is reached
+{
+    time_t treshold;
+    while (start < end) {
+        treshold = search(start, start + delta);
+        if (treshold) {
+            start = treshold;
+            print_time(treshold);
+        } else {
+            start = start + delta;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 4) return 1;
+    time_t start, end, delta;
+    start = (time_t)strtoull(argv[1], NULL, 10);
+    end =   (time_t)strtoull(argv[2], NULL, 10);
+    delta = (time_t)strtoull(argv[3], NULL, 10);
+
+    multisearch(start, end, delta);
+
+    return 0;
+}
